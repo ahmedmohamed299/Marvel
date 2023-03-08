7252694f806d70080d581b871167f81bb347c34e
ahmedAbdElSalam299 <aabdelsalam@anspire.agency>
2023-03-08 23:34:09 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marvel/presentation/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.marvel.presentation.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.marvel.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
